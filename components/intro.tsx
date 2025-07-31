86efb5cde0a37aedc35d1d0ad6edac8418156784
Edafe Oke Great <greatedafeoke@gmail.com>
2025-07-31 21:37:32 +0100

    Added email support

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import authorImage from '@/public/images/authors/edafeoke.png'
+import authorImage from '@/public/images/authors/edafe-oke.png'
 
 export default function Intro() {
   return (
